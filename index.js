96c4a9616e39ec9b005917bf9c0b74d91a756c9e
sk8702320 <sk8702320@gmail.com>
2022-01-20 23:06:24 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,5 @@
+document.getElementById("image").onclick = function() {
+    this.src = "image.jpg";
+    document.getElementById('image').style.width = "500px";
+    document.getElementById('image').style.height = "500px";
+};
